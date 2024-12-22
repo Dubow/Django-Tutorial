42c656a6261088527f146130c3aeb007c41f96c9
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-22 21:59:56 +0800

    making debug = False in the settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w2+!(v-v*gi4a163j6fm_cs*)xhru357gd!9s+++p#b49=fgad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

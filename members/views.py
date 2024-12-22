f26657996e3bb636f29ce846f8b01051bbaeb3a4
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-22 21:44:27 +0800

    updating views.py file

--- members/views.py
+++ members/views.py
@@ -18,3 +18,6 @@ def details(request, id):
         'mymember' : mymember
     }
     return HttpResponse(template.render(context, request))
+def main(request):
+    template = loader.get_template("main.htlm")
+    return HttpResponse(template.render())
